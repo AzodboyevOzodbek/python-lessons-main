499be155ddf634b6979d97e06a4cd0e0c7136c44
azodbooyevozodbek <ozodboyev0609@gmail.com>
2026-03-26 16:27:52 +0500

    feat: add function examples for greeting and parameter usage

---
+++ function.py
@@ -0,0 +1,12 @@
+# Function - ma'lum bir vazifani bajaruvchi kod bloklari
+# ularni qayta ishlatish mumkin va ular dastur tuzilishini yaxshilaydi
+# def salom_ber():
+#     print("Assalomu alaykum!")
+
+# salom_ber()  # Funksiyani chaqirish
+
+# 2. Funksiyaga qiymat uzatish
+# def salom_ber(ism):
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+
+# salom_ber('hasan')
