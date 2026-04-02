859c44cc1e1a60afb844ddf0b0ee7947fb970a84
azodbooyevozodbek <ozodboyev0609@gmail.com>
2026-04-02 16:21:23 +0500

    feat: add return function examples for addition and even number check

---
+++ return-function.py
@@ -0,0 +1,24 @@
+# def add_numbers(a, b):
+#     print(a + b)
+
+# add_numbers(5, 10)
+
+# def add_numbers(a, b):
+#     return a + b
+
+# result = add_numbers(5, 10)
+# print(result)
+
+# def isEven(son):
+#     if son % 2 == 0:
+#         return "juft"
+#     else:    
+#         return "toq"
+
+# natija = isEven(son=int(input("Son kiriting: ")))
+# print(natija)
+
+# def isEven(son):
+#     return "juft" if son % 2 == 0 else "toq"
+# natija = isEven(son=int(input("Son kiriting: ")))
+# print(natija)
