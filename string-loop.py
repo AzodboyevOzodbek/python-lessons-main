0e6c517a16117d5d6fa39129fa1abb9ef5961125
azodbooyevozodbek <ozodboyev0609@gmail.com>
2026-04-04 16:09:24 +0500

    feat: add string iteration examples using for loops

---
+++ string-loop.py
@@ -0,0 +1,17 @@
+text = "Python lessons"
+# print(len(text)) #14
+# string ichidagi har bir belgi indexlanadi va shu bilan birga bo'sh joy ham hisoblanadi
+# print(text[0]) #P
+# print(text[1]) #y
+# print(text[2]) #t
+
+# print(text[-1]) #s
+# print(text[-2]) #n
+
+# 1 - usul
+for belgi in text:
+    print(belgi)
+# 2 - usul
+for index in range(len(text)):
+    print(index)
+# 3 - usul
